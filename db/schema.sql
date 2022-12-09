d85e263af26fee8be7faa1817657ce59288b5b83
Maddrick Long <110777599+Drip101@users.noreply.github.com>
2022-12-08 19:52:26 -0500

    schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,16 @@
-DROP DATABASE IF EXISTS crowdfund_db;
-CREATE DATABASE crowdfund_db;
+DROP DATABASE IF EXISTS grocery_db;
+CREATE DATABASE grocery_db;
+
+USE grocery_db;
+
+CREATE TABLE products(
+  id INT NOT NULL,
+  product_name VARCHAR(30) NOT NULL,
+  category_name VARCHAR(100) NOT NULL,
+  
+);
+
+CREATE TABLE categories(
+  id INT NOT NULL,
+  category_name 
+);
